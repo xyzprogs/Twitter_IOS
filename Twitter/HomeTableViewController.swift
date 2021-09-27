fd418124646dc7318853164821eaa745f06556ad
Andy <zhengxinyao888@gmail.com>
2021-09-26 22:34:38 -0400

    designed the tweet view cell

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -23,14 +23,22 @@ class HomeTableViewController: UITableViewController {
         
     }
     // MARK: - Table view data source
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+        
+        cell.userNameLabel.text = "name"
+        cell.tweetContent.text = "Something Else"
+        
+        return cell
+    }
     override func numberOfSections(in tableView: UITableView) -> Int {
        
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 }
