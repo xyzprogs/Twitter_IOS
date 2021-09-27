74d0dd99a02282bf50a0d320d25a543070372816
Andy <zhengxinyao888@gmail.com>
2021-09-26 21:58:18 -0400

    added a log out button

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeTableViewController.swift
+//  HomeTableViewController
+//
+//  Created by Andy Zheng on 9/26/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+       
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
